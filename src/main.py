3f458b242e5164b45b10df0b0f8f8f5fc6e81d9d
hrokr <alex.rook@gmail.com>
2022-07-31 21:00:39 -0400

    Changes of unknown origin

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 import os
-
-from quake_data import get_quake_json 
-
-
+import pandas as pd
+from quake_data import get_data
 
 
 if __name__ == "__main__":
-    get_quake_json()
+    #get_quake_json()
+    get_data(df)
