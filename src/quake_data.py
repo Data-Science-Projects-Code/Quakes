154cba2262c54689a7dc457664a4f99f155959f5
hrokr <alex.rook@gmail.com>
2022-09-19 22:23:53 -0400

    Fixed function for  module

--- src/quake_data.py
+++ src/quake_data.py
@@ -1,12 +1,8 @@
-from tkinter import W
 import requests
 import pandas as pd
 from pandas.io.json import json_normalize
 
-def get_data(df):
+def get_quake_data():
     data = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson")
-    # data = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojson")
-
     jsondata = data.json()
-    quakes = pd.json_normalize(jsondata['features'])
-    print(df)
+    return json_normalize(jsondata['features'])
