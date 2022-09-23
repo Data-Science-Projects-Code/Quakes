c26c5aec6c7c59d05fb88019e373d7f6222bbe93
hrokr <alex.rook@gmail.com>
2022-09-22 22:52:12 -0400

    A change, yes. Hmmm. Remember it. I do not

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import os
 import pandas as pd
-from quake_data import get_quake_data
+from quake_data import get_quake_data, remove_prefixes, remove_others 
 
 
 if __name__ == "__main__":
-    df = get_quake_data()
+    quake_data = get_quake_data()
+    df = remove_prefixes(quake_data)
     print(df)
+    #print(cleaned_data)
+
