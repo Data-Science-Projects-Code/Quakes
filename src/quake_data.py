eec5065bea97ba5a59890e5db0d4906380351b2c
hrokr <alex.rook@gmail.com>
2022-09-24 22:58:52 -0400

    Done for -- for the night

--- src/quake_data.py
+++ src/quake_data.py
@@ -1,8 +1,21 @@
 import requests
 import pandas as pd
-from pandas.io.json import json_normalize
 
 def get_quake_data():
     data = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson")
     jsondata = data.json()
-    return json_normalize(jsondata['features'])
+    return pd.json_normalize(jsondata['features'])
+
+def remove_prefixes(df):
+    print(df.columns)
+    v2 = df.columns.str.replace('properties.', "", regex=False)
+    v3 = v2.columns.str.replace('geometry.', "", regex=False)
+    print(v2)
+    print("and now for the last step")
+    print(v3)
+    return(df)
+    
+def drop_useless_cols(df):    
+    # quake_data.drop(['id', 'type', 'updated', 'tz', 'mmi', 'detail', 'felt','cdi', 'felt', 'types', 'nst', 'type', 'title'], axis=1, inplace=True)
+    #print(quake_data.columns)
+    return quake_data
