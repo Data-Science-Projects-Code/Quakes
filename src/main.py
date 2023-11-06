7bf95a1c19e7b217523516fa4f225b3485dc5ae3
hrokr <alex.rook@gmail.com>
2023-11-06 03:37:56 -0500

    refactored main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import os
 import pandas as pd
-from quake_data import get_quake_data, remove_prefixes, remove_others 
-
+from quake_data import get_quake_data, remove_prefixes, drop_useless_cols
 
 if __name__ == "__main__":
     quake_data = get_quake_data()
-    df = remove_prefixes(quake_data)
-    print(df)
-    #print(cleaned_data)
+    print("Data fetched")
 
+    df = remove_prefixes(quake_data)
+    print("Initial formatting done")
+
+    df = drop_useless_cols(df)
+    print("Data cleaned. This is what we'll be working with")
+    print(df)
