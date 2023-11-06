4adc250f205d0e84c242e1d09bfd8c84953f73e3
hrokr <alex.rook@gmail.com>
2023-11-06 03:37:12 -0500

    refactored and debug quake_data

--- src/quake_data.py
+++ src/quake_data.py
@@ -1,21 +1,19 @@
 import requests
 import pandas as pd
 
+
 def get_quake_data():
     data = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson")
     jsondata = data.json()
-    return pd.json_normalize(jsondata['features'])
+    df = pd.json_normalize(jsondata["features"])
+    return df
 
 def remove_prefixes(df):
-    print(df.columns)
-    v2 = df.columns.str.replace('properties.', "", regex=False)
-    v3 = v2.columns.str.replace('geometry.', "", regex=False)
-    print(v2)
-    print("and now for the last step")
-    print(v3)
-    return(df)
-    
-def drop_useless_cols(df):    
-    # quake_data.drop(['id', 'type', 'updated', 'tz', 'mmi', 'detail', 'felt','cdi', 'felt', 'types', 'nst', 'type', 'title'], axis=1, inplace=True)
-    #print(quake_data.columns)
-    return quake_data
+    return df.rename(columns=lambda x: x.replace("properties.", "").replace("geometry.", ""))
+
+def drop_useless_cols(df):
+    columns_to_drop = [
+        "id", "type", "updated", "tz", "mmi", "detail", "felt", "cdi",
+        "felt", "types", "nst", "type", "title"
+    ]
+    return df.drop(columns=columns_to_drop)
