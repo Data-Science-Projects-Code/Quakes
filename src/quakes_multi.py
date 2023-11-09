9f862576bb78645e0da616562049e5a12b1b3477
hrokr <alex.rook@gmail.com>
2023-11-09 14:14:19 -0500

    Verify both sides are on the same page of the script

--- src/quakes_multi.py
+++ src/quakes_multi.py
@@ -1,6 +1,6 @@
 from transfer_data import check_for_data, get_quake_data, save_data
 from mung_data import remove_prefixes, drop_useless_cols, strip_commas, convert_to_datetime
-from format_data import splitLatLonDepth
+from format_data import splitLatLonDepth, conv_to_dd#dms2dd, 
 
 if __name__ == "__main__":
     if check_for_data():
@@ -13,7 +13,11 @@ if __name__ == "__main__":
         df = drop_useless_cols(df)
         df['ids'], df['sources'] = strip_commas(df['ids'], df['sources'])
         df = convert_to_datetime(df)
+        print("databefore splitting fields")
+        print(df)
         df = splitLatLonDepth(df)
+        print(df)
+        df = conv_to_dd(df['latitude'])
         print("Data cleaned. This is what we'll be working with")
         print(df)
 
