e23313b0235307dcd01681d3fa3d956763fbfa33
hrokr <alex.rook@gmail.com>
2023-11-13 15:14:54 -0500

    Add: Title and minimal styling

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,25 +1,29 @@
 import pandas as pd
 import plotly.express as px
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 
 df = pd.read_pickle('../data/quakes_last_24.pkl')
 app = dash.Dash(__name__)
 
+# Add external stylesheets
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
+    html.H1('Earthquakes last 24 hours', style={'textAlign': 'center'}),  # Add a title
     dcc.Graph(id='map', style={'height': '700px'}), 
-    dcc.RangeSlider(
+    html.Div(dcc.RangeSlider(
         id='mag-slider',
         min=2.5,
         max=9.0,
         step=0.1,
         value=[2.5, 9.0],
-        #marks={i: {'label': str(i), 'style': {'transform': 'rotate(45deg)'}}
+        marks={i: {'label': str(i), 'style': {'transform': 'rotate(45deg)'}}
                     for i in range(3, 10)}
-    ),
+    ), style={'width': '80%', 'margin': 'auto'}),  # Make the slider the length of the map
     dcc.RadioItems(
         id='tsunami-filter',
         options=[{'label': 'All', 'value': 'all'},
@@ -27,14 +31,12 @@ app.layout = html.Div([
                  {'label': 'No Tsunami Warning', 'value': 'no'}],
         value='all',
     )
-])
-
+], style={'backgroundColor': '#FAFAFA'})  # Change the background color
 
 @app.callback(
     Output('map', 'figure'),
     [Input('mag-slider', 'value'),
      Input('tsunami-filter', 'value')])
-
 
 def update_map(mag_range, tsunami):
     filtered_df = df[(df['mag'] >= mag_range[0]) & (df['mag'] <= mag_range[1])]
@@ -48,11 +50,11 @@ def update_map(mag_range, tsunami):
         size = 'mag',
         color = 'tsunami',
         opacity = .3,
-        width = ['orange', 'blue'],
+        #width = ['orange', 'blue'],
         hover_name = 'place',
         projection = 'natural earth')
     return fig
 
-
 if __name__ == '__main__':
     app.run_server(debug=True)
+
