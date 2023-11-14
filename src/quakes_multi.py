272e13ffed1a071f46ec3bfe3ac0fb23419c0543
hrokr <alex.rook@gmail.com>
2023-11-14 16:07:59 -0500

    Removed useless code

--- src/quakes_multi.py
+++ src/quakes_multi.py
@@ -1,6 +1,6 @@
-from transfer_data import check_for_data, get_quake_data, save_data
-from mung_data import remove_prefixes, drop_useless_cols, strip_commas, convert_to_datetime
-from format_data import splitLatLonDepth, conv_to_dd#dms2dd, 
+from imports.transfer_data import check_for_data, get_quake_data, save_data
+from imports.mung_data import remove_prefixes, drop_useless_cols, strip_commas, convert_to_datetime, fix_tsunami
+from imports.format_data import splitLatLonDepth
 
 if __name__ == "__main__":
     if check_for_data():
@@ -11,13 +11,11 @@ if __name__ == "__main__":
 
         df = remove_prefixes(quake_data)
         df = drop_useless_cols(df)
+        df = fix_tsunami(df)
         df['ids'], df['sources'] = strip_commas(df['ids'], df['sources'])
         df = convert_to_datetime(df)
-        print("databefore splitting fields")
-        print(df)
         df = splitLatLonDepth(df)
         print(df)
-        df = conv_to_dd(df['latitude'])
         print("Data cleaned. This is what we'll be working with")
         print(df)
 
