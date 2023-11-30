8bded41ba09019b590a5861e390f19a9a3e6ab28
hrokr <alex.rook@gmail.com>
2023-11-30 14:40:33 -0500

    Change: refactor to use Bootstrap

--- src/dashboard.py
+++ src/dashboard.py
@@ -8,95 +8,105 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 
 df = pd.read_pickle('../data/quakes_last_24.pkl')
 geo_df = gpd.read_file('../data/GeoJSON/PB2002_boundaries.json')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = html.Div([
-    html.H1('Earthquakes over the Last 24 Hours', style={'textAlign': 'center'}),
-    html.Div([
-        html.H2('World View', style={'textAlign': 'center'}),
-        dcc.Graph(id='map', style={'height': '700px', 'padding': '2px'}),
-        html.Div(dcc.RangeSlider(
-            id='mag-slider',
-            min=2.5,
-            max=9.0,
-            step=0.1,
-            value=[2.5, 9.0],
-            marks={i: {'label': str(i), 'style': {'transform': 'rotate(0deg)', 'font-size': '20px'}}
-                        for i in range(3, 10)},
-            included=True
-        ), style={'width': '100%', 'margin': 'auto', 'padding': '2px'}),
-
-        html.Div([
-            dcc.RadioItems(
-                id='tsunami-filter',
-                options=[{'label': 'All', 'value': 'all'},
-                         {'label': 'Tsunami Warning Issued', 'value': 'yes'},
-                         {'label': 'No Tsunami Warning', 'value': 'no'}],
-                value='all',
-                inline=True
-            ),
-            dcc.RadioItems(
-                id='boundary-toggle',
-                options=[{'label': 'Show Major Fault Lines', 'value': 'show'},
-                         {'label': 'Hide Major Fault Lines', 'value': 'hide'}],
-                value='show',
-                inline=True
-            )
-        ], style={'padding': '2px'})
-    ], style={'width': '66%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '2px'}),
-
-
-    html.Div([
-        html.H2('Today\'s Earthquakes Sorted By', style={'textAlign': 'center'}),
-        
-        html.H4([
-        dcc.RadioItems(
-            id='sort-by',
-                options=[{'label': 'Magnitude', 'value': 'mag'},
-                         {'label': 'Time', 'value': 'datetime'},
-                         {'label': 'Depth', 'value': 'depth'},
-
-                         ],
-            value='datetime',
-            inline=True
-        ),
-        dcc.RadioItems(
-            id='sort-order',
-            options=[{'label': 'Ascending', 'value': 'asc'},
-                     {'label': 'Descending', 'value': 'desc'}],
-            value='desc',
-            inline=True
-        )]),
-        html.Div(id='quake-details', style={'overflow': 'auto', 'height': '670px', 'padding':'10px'})
-    ], style={'width': '33%', 'display': 'inline-block', 'vertical-align': 'top', 'padding': '2px'})
-], style={'backgroundColor': '#FAFAFA'})
-
-'''
-@app.callback(
-    Output('map', 'figure'),
-    [Input('mag-slider', 'value'),
-     Input('tsunami-filter', 'value'),
-     Input('boundary-toggle', 'value')])
-def update_map(mag_range, tsunami, boundary):
-    filtered_df = df[(df['mag'] >= mag_range[0]) & (df['mag'] <= mag_range[1])]
-    if tsunami != 'all':
-        filtered_df = filtered_df[filtered_df['tsunami warning'] == (1 if tsunami == 'yes' else 0)]
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.H1('Earthquakes over the Last 24 Hours', className='text-center mb-4'), width=12),
+    ]),
     
-    fig = px.scatter_geo(filtered_df,
-        lat = 'latitude',
-        lon = 'longitude',
-        size = 'mag',
-        color = 'tsunami warning',
-        opacity = .3,
-        color_discrete_map = [True: 'blue', False: 'orange'],
-        hover_name = 'place',
-        projection = 'natural earth')
-'''
+    dbc.Row([
+        dbc.Col([
+            html.H2('World View', className='text-center mb-3'),
+            dcc.Graph(
+                id='map',
+                style={'height': '700px'},
+                clickData={'points': []}
+            ),
+            dbc.Row([
+                dbc.Col([
+                    html.Label('Magnitude Range'),
+                    dcc.RangeSlider(
+                        id='mag-slider',
+                        min=2.5,
+                        max=9.0,
+                        step=0.1,
+                        value=[2.5, 9.0],
+                        marks={i: {'label': str(i), 'style': {'transform': 'rotate(0deg)', 'font-size': '12px'}}
+                               for i in range(3, 10)},
+                        included=True,
+                    ),
+                ], className='mt-3'),
+            ]),
+            dbc.Row([
+                dbc.Col([
+                    dbc.RadioItems(
+                        id='tsunami-filter',
+                        options=[
+                            {'label': 'All', 'value': 'all'},
+                            {'label': 'Tsunami Warning Issued', 'value': 'yes'},
+                            {'label': 'No Tsunami Warning', 'value': 'no'}
+                        ],
+                        value='all',
+                        inline=True
+                    ),
+                ], className='mt-3'),
+                dbc.Col([
+                    dbc.RadioItems(
+                        id='boundary-toggle',
+                        options=[
+                            {'label': 'Show Major Fault Lines', 'value': 'show'},
+                            {'label': 'Hide Major Fault Lines', 'value': 'hide'}
+                        ],
+                        value='show',
+                        inline=True
+                    ),
+                ], className='mt-3'),
+            ]),
+        ], width=8),
+
+    dbc.Col([
+        html.H2('Today\'s Earthquakes Sorted By', className='text-center mb-3'),
+        dbc.Row([
+            dbc.Col([
+                dbc.RadioItems(
+                    id='sort-by',
+                    options=[
+                        {'label': 'Magnitude', 'value': 'mag'},
+                        {'label': 'Time', 'value': 'datetime'},
+                        {'label': 'Depth', 'value': 'depth'},
+                    ],
+                    value='datetime',
+                    inline=True
+                ),
+            ], width=6),
+            dbc.Col([
+                dbc.RadioItems(
+                    id='sort-order',
+                    options=[
+                        {'label': 'Ascending', 'value': 'asc'},
+                        {'label': 'Descending', 'value': 'desc'}
+                    ],
+                    value='desc',
+                    inline=True
+                ),
+            ], width=6),
+        ]),
+        dbc.ListGroup(id='quake-details', style={'overflow': 'auto', 'max-height': '670px', 'padding': '10px'}),
+    ], width=4),
+])
+ 
+
+], fluid=True, style={'backgroundColor': '#FAFAFA'})
+
+
+
 
 @app.callback(
     Output('map', 'figure'),
@@ -117,6 +127,7 @@ def update_map(mag_range, tsunami, boundary):
         color_discrete_map={True: 'blue', False: 'orange'},  # Map True to blue and False to orange
         hover_name='place',
         projection='natural earth')
+
     if boundary == 'show':
         lats = []
         lons = []
@@ -142,15 +153,6 @@ def update_map(mag_range, tsunami, boundary):
 
     return fig
 
-"""
-@app.callback(
-    Output('quake-details', 'children'),
-    [Input('sort-by', 'value'),
-     Input('sort-order', 'value')])
-def update_quake_details(sort_by, sort_order, tsunami):
-    sorted_df = df.sort_values(sort_by, ascending=(sort_order == 'asc'))
-    return html.Ul([html.Li(f"{row['place']}, {row['datetime']}, {row['mag']}") for _, row in sorted_df.iterrows()])
-"""
 
 @app.callback(
     Output('quake-details', 'children'),
